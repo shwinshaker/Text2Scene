dbd60f31b1b4d32881266eb9a3330ed13b4fca92
shwinshaker <740992427@qq.com>
2019-08-14 13:45:00 +0800

    2.0stuff

---
+++ rules/labels.py
@@ -0,0 +1,6 @@
+#!./env python
+
+subjects = dict()
+subjects['character'] = ['man', 'woman', 'alien', 'group']
+subjects['surrounding'] = ['office', 'home', 'park', 'wild', 'street', 'city',
+                           'chart', 'website', 'other']
